a97bd354d3fadc0fb3d40db02113a2f92a5f74e3
Ziwei Liu <790222124@qq.com>
2020-10-13 09:48:37 +0800

    Update SPA.py

--- SPA.py
+++ SPA.py
@@ -12,6 +12,7 @@
 # numpy : numpy  1.16.2
 # scipy : scipy  1.4.1
 # progress:progress    1.5
+'''在原作者代码上做了一些修改'''
 
 import pandas as pd
 import numpy as np
@@ -21,9 +22,7 @@ import scipy.io as scio
 from progress.bar import Bar
 from matplotlib import pyplot as plt
 
-
 class SPA:
-
     def _projections_qr(self, X, k, M):
         '''
         原版连续投影算法使用MATLAB内置的QR函数
@@ -32,23 +31,17 @@ class SPA:
         X : 预测变量矩阵
         K ：投影操作的初始列的索引
         M : 结果包含的变量个数
-
         return ：由投影操作生成的变量集的索引
         '''
-
         X_projected = X.copy()
-
         # 计算列向量的平方和
         norms = np.sum((X ** 2), axis=0)
         # 找到norms中数值最大列的平方和
         norm_max = np.amax(norms)
-
         # 缩放第K列 使其成为“最大的”列
         X_projected[:, k] = X_projected[:, k] * 2 * norm_max / norms[k]
-
         # 矩阵分割 ，order 为列交换索引
         _, __, order = qr(X_projected, 0, pivoting=True)
-
         return order[:M].T
 
     def _validation(self, Xcal, ycal, var_sel, Xval=None, yval=None):
@@ -63,12 +56,10 @@ class SPA:
             NV = Xval.shape[0]  # NV 验证集的个数
 
         yhat = e = None
-
         # 使用单独的验证集进行验证
         if NV > 0:
             Xcal_ones = np.hstack(
                 [np.ones((N, 1)), Xcal[:, var_sel].reshape(N, -1)])
-
             # 对偏移量进行多元线性回归
             b = np.linalg.lstsq(Xcal_ones, ycal, rcond=None)[0]
             # 对验证集进行预测
@@ -107,27 +98,19 @@ class SPA:
         如果 m_max 为空时：
             1. 当使用单独的验证集进行验证时， m_max = min(N-1, K)
             2. 当使用交叉验证时，m_max = min(N-2, K)
-
         autoscaling : 是否使用自动刻度 yes = 1，no = 0, 默认为 1
-
         '''
-
         assert (autoscaling == 0 or autoscaling == 1), "请选择是否使用自动计算"
-
         N, K = Xcal.shape
-
         if m_max is None:
             if Xval is None:
                 m_max = min(N - 1, K)
             else:
                 m_max = min(N - 2, K)
-
         assert (m_max < min(N - 1, K)), "m_max 参数异常"
 
         # 第一步： 对测试集进行投影操作
-
         # 在均值中心化 和 自动窗口 之后 对 Xcal的列进行投影操作
-
         normalization_factor = None
         if autoscaling == 1:
             normalization_factor = np.std(
@@ -141,7 +124,6 @@ class SPA:
             Xcaln[:, k] = (x - np.mean(x)) / normalization_factor[k]
 
         SEL = np.zeros((m_max, K))
-
         # 进度条
         with Bar('Projections :', max=K) as bar:
             for k in range(K):
@@ -149,9 +131,7 @@ class SPA:
                 bar.next()
 
         # 第二步： 进行评估
-
         PRESS = float('inf') * np.ones((m_max + 1, K))
-
         with Bar('Evaluation of variable subsets :', max=(K) * (m_max - m_min + 1)) as bar:
             for k in range(K):
                 for m in range(m_min, m_max + 1):
@@ -159,16 +139,13 @@ class SPA:
                     _, e = self._validation(Xcal, ycal, var_sel, Xval, yval)
                     PRESS[m, k] = e.T.dot(e)
                     bar.next()
-
         PRESSmin = np.min(PRESS, axis=0)
         m_sel = np.argmin(PRESS, axis=0)
         k_sel = np.argmin(PRESSmin)
-
-        # 第 k_sel波段为初始波段时最佳，波段数目为 m_sel（k_sel）
+        # 第 k_sel 波段为初始波段时最佳，波段数目为 m_sel(k_sel)
         var_sel_phase2 = SEL[:m_sel[k_sel], k_sel].astype(np.int)
 
         # 最后消去变量
-
         # 第 3.1 步 计算相关指数
         Xcal2 = np.hstack([np.ones((N, 1)), Xcal[:, var_sel_phase2]])
         b = np.linalg.lstsq(Xcal2, ycal, rcond=None)[0]
@@ -203,7 +180,6 @@ class SPA:
         i_crit = max(m_min, i_crit)
 
         var_sel = var_sel_phase2[index_decreasing_relev[:i_crit]]
-
         plt.rcParams['font.sans-serif'] = ['SimHei']  # 用来正常显示中文标签
         plt.rcParams['axes.unicode_minus'] = False  # 用来正常显示负号
         fig1 = plt.figure()
@@ -221,13 +197,19 @@ class SPA:
         plt.xlabel('Variable index')
         plt.grid(True)
         plt.show()
-
         return var_sel, var_sel_phase2
 
     def __repr__(self):
         return "SPA()"
 
 
+'''
+（1）变量Xcal，Ycal指的是（Xcalibration和Ycalibration）, Xval个Yval指的是（Xvalidation和Yvalidation）,
+    从变量的命名可以看出，Xcal和Ycal是需要计算的光谱矩阵（训练集），Xval和Yval是验证的光谱矩阵（测试集）。
+（2）Xcal（训练集矩阵）和Xval（测试集矩阵）都是M*N的光谱矩阵（M为样本数，N为维度（波段））。
+（3）Ycal和Yval在程序里没有注释，不是很清楚含义。Ycal和Yval都是M*1的维的矩阵，应该是是训练集和测试集的训练标签。
+（4）分析前光谱数据需要平滑去噪，否则误差较大。
+'''
 if __name__ == "__main__":
     data = pd.read_excel(r"F:\实验\data\all.xlsx")
     x = data.drop(['names', 'labels'], axis=1)
@@ -246,6 +228,11 @@ if __name__ == "__main__":
     Xcal = min_max_scaler.fit_transform(Xcal)
     Xval = min_max_scaler.transform(Xval)
    
+    # var_sel, var_sel_phase2 = SPA().spa(
+    #     Xcal, ycal, m_min=2, m_max=50, Xval=Xval, yval=yval, autoscaling=1)
     var_sel, var_sel_phase2 = SPA().spa(
         Xcal, ycal, m_min=2, m_max=50, Xval=Xval, yval=yval, autoscaling=1)
     print(absorbances[var_sel])
+
+
+
